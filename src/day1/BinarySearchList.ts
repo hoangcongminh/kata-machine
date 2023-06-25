5fc859629ba1b375b2a93b4336747090f66933fa
congminh <congminh2320@gmail.com>
2023-06-25 12:43:59 +0700

    implement LinearSearchList & BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,40 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    var lo = 0;
+    var hi = haystack.length - 1;
 
+    while (lo <= hi) {
+        const middle = Math.floor(lo + (hi - lo) / 2);
+        const value = haystack[middle];
+
+        if (value == needle) {
+            return true;
+        } else if (value < needle) {
+            lo = middle + 1;
+        } else {
+            hi = middle - 1;
+        }
+    }
+
+    return false;
 }
+
+//case take (lo, hi]
+// export default function bs_list(haystack: number[], needle: number): boolean {
+//     var lo = 0;
+//     var hi = haystack.length;
+
+//     do {
+//         const middle = Math.floor(lo + (hi - lo) / 2);
+//         const value = haystack[middle];
+
+//         if (value == needle) {
+//             return true;
+//         } else if (value < needle) {
+//             lo = middle + 1;
+//         } else {
+//             hi = middle;
+//         }
+//     } while (lo < hi);
+
+//     return false;
+// }
