f2e638780b11c39095c528e62a5ba46cd7d072d6
congminh <congminh2320@gmail.com>
2023-07-10 14:13:59 +0700

    update BubbleSort implement

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,11 +1,17 @@
 export default function bubble_sort(arr: number[]): void {
     for (let i = 0; i < arr.length; ++i) {
+        let isSwapFlag = false;
         for (let j = 0; j < arr.length - 1 - i; ++j) {
             if (arr[j] > arr[j + 1]) {
+                isSwapFlag = true;
                 const temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
+        }
+        // If no swap, then the array is sorted.
+        if (!isSwapFlag) {
+            return;
         }
     }
 }
