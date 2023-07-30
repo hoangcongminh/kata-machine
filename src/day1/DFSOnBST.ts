e052b7e79a78ac2e54f24ad27d22e5b1e45558a3
congminh <congminh2320@gmail.com>
2023-07-30 16:41:33 +0700

    implement DFSOnBST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,18 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(head: BinaryNode<number> | null, needle: number): boolean {
+    if (!head) {
+        return false;
+    }
 
+    if (head.value === needle) {
+        return true;
+    }
+
+    if (head.value > needle) {
+        return search(head.left, needle);
+    }
+    return search(head.right, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
